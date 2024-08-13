4fd08c8153aeb5e251112f3266d6c8bb3de6e38f
bmkim <bmkim@dozn.co.kr>
2024-08-14 08:57:48 +0900

    [Feature] add email send function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-import puppeteer from 'puppeteer';import { loginToSite, navigateToPage, selectLotteryNumbers, confirmAndBuy } from './operations';import { LOGIN_PAGE, PURCHASE_PAGE } from './constant';const initPuppeteer = async (): Promise<void> => {    const browser = await  puppeteer.launch({        headless: true,        args: ['--no-sandbox', '--disable-setuid-sandbox']    });    const page = await browser.newPage();    await navigateToPage(page, LOGIN_PAGE);    await loginToSite(page, "kbm19940526", "Qudals12!@");    await navigateToPage(page, PURCHASE_PAGE);    await selectLotteryNumbers(page);    await confirmAndBuy(page);    await page.screenshot({ path: 'screenshot.png', fullPage: true })    await browser.close();};await initPuppeteer()
+import puppeteer from 'puppeteer';
+import { LOGIN_PAGE, PURCHASE_PAGE } from './constant';
+import { confirmAndBuy, loginToSite, navigateToPage, selectLotteryNumbers, sendEmail } from './operations';
+
+(async (): Promise<void> => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+
+  await navigateToPage(page, LOGIN_PAGE);
+  await loginToSite(page, 'kbm19940526', 'Qudals12!@');
+
+  await navigateToPage(page, PURCHASE_PAGE);
+  await selectLotteryNumbers(page);
+  await confirmAndBuy(page);
+
+  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+
+  await browser.close();
+
+  await sendEmail();
+})();
--- src/operations.ts
+++ src/operations.ts
@@ -1,25 +1,27 @@
+import nodemailer from 'nodemailer';
+import path from 'path';
 import { Page } from 'puppeteer';
 
-export const navigateToPage = async (page :  Page, url: string) => {
+export const navigateToPage = async (page: Page, url: string) => {
   await page.goto(url);
-}
+};
 
 export const loginToSite = async (page: Page, userId: string, password: string): Promise<void> => {
-  await page.click("#userId");
+  await page.click('#userId');
   await page.type('#userId', userId);
 
   await page.click('[title="비밀번호"]');
   await page.type('[title="비밀번호"]', password);
 
-  await page.keyboard.press("Enter");
+  await page.keyboard.press('Enter');
   await page.waitForNavigation();
 };
 
 export const selectLotteryNumbers = async (page: Page): Promise<void> => {
   await page.click('#num2');
   await page.click('#amoundApply');
-  await page.keyboard.press("1");
-  await page.keyboard.press("Enter");
+  await page.keyboard.press('1');
+  await page.keyboard.press('Enter');
   await page.click('#btnSelectNum');
 };
 
@@ -28,7 +30,30 @@ export const confirmAndBuy = async (page: Page): Promise<void> => {
   await page.evaluate(() => {
     const confirmButtonSelector = '.box .btns input[value="확인"]';
     const buttons = Array.from(document.querySelectorAll(confirmButtonSelector)) as HTMLElement[];
-    const visibleButtons = buttons.filter(button => button.offsetParent !== null);
-    visibleButtons.forEach(button => button.click());
+    const visibleButtons = buttons.filter((button) => button.offsetParent !== null);
+    visibleButtons.forEach((button) => button.click());
+  });
+};
+
+export const sendEmail = async () => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `byungminkim12@gmail.com`,
+    to: 'byungminkim12@gmail.com', // 받을 이메일 주소
+    subject: '복권 구매', // 이메일 제목
+    text: `${Date.now()} 구매`, // 이메일 내용
+    attachments: [
+      {
+        filename: 'image.png', // 첨부 파일 이름
+        path: path.join(__dirname, 'screenshot.png'), // 로컬 경로에 있는 파일
+      },
+    ],
   });
 };
